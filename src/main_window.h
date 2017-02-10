5f1e4712fdbacf1bdc36a2fecfad8a59aedf30c1
Rodrigo Reichert <reichert.rodrigo@gmail.com>
2017-02-10 18:24:55 +1100

    improved output format for roots

--- src/main_window.h
+++ src/main_window.h
@@ -9,8 +9,34 @@ class MainWindow : public QMainWindow
 	Q_OBJECT
 
 	public:
+
+		/**
+		 * Default Constructor
+		 *
+		 * @param parent parent widget who will act as the parent for this widget
+		 */
 		explicit MainWindow(QWidget *parent = nullptr);
+
+		/**
+		 * Deconstructor
+		 */
 		~MainWindow();
+
+	private slots:
+
+		/**
+		 * Signal method which will be called on every time the user changes the value
+		 * of the coefficients.
+		 *
+		 * The method will use the quadratic equation below to solve for the roots:
+		 *
+		 * \f[
+		 *
+		 *   x = \frac{-b \pm \sqrt{b^2 - 4ac}} {2a}
+		 *
+		 * \f]
+		 */
+		void update_equation();
 
 	private:
 		Ui::MainWindow *ui;
